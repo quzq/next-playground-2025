a02d3b301bfd6ec39e5dfc03b6c3f7758d0479d6
quzq <user@MSI>
2025-02-15 12:14:52 +0900

    revice deploy script

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/next-playground-2025",
+  assetPrefix: "/next-playground-2025/",
+  images: {
+    unoptimized: true, // 画像の最適化はオフにする（静的エクスポートの場合）
+  },
   /* config options here */
 };
 
